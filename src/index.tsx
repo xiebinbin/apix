853aec78f350a574ea3ab793df5e2a8027db5706
soda <soda@sodadeMacBook-Air.local>
2025-02-22 11:33:53 +0800

    x

--- src/index.tsx
+++ src/index.tsx
@@ -43,7 +43,8 @@ app.post('/api/ad-statistic/status', zValidator('json', z.object({
   const runRecord = await AdStatisticService.getLastRecordRunLog(packageName, adId, c.var.uuid);
   let result = runRecord ? (runRecord.status == 1 ? false : true) : false;
   console.log("runRecord", runRecord)
-  if (!result) {
+  console.log("result", result)
+  if (result) {
     const record = await AdStatisticService.getLatest(adId, packageName);
     result = record?.successCount ? record?.successCount < process.env.MAX_REQUEST_COUNT : true;
   }
